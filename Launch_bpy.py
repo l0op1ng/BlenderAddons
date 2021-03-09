2feac9c50c5893daf1f1eb080011c3ab166e7958
Rob <loop1ng@me.com>
2021-03-09 13:54:43 -0500

    adding a test blend file and and launch file for blender to execute a local script

--- Launch_bpy.py
+++ Launch_bpy.py
@@ -1,2 +1,12 @@
-filename = "F:/Projects/Git/BlenderAddons/learning.py"
+# single file
+filename = "F:/Projects/Git/BlenderAddons/op_boolean.py"
 exec(compile(open(filename).read(), filename, 'exec'))
+
+# script in a module
+"""
+import myscript
+import importlib
+
+importlib.reload(myscript)
+myscript.main()
+"""
--- op_boolean.py
+++ op_boolean.py
@@ -10,6 +10,14 @@ def boolean_operation(objTool):
     bpy.ops.object.modifier_add(type='BOOLEAN')
     bool_modifier = bpy.context.active_object.modifiers[-1]
     bool_modifier.name = "bool_modifier"
+    bpy.ops.object.modifier_move_up(modifier=bool_modifier.name)
+    bool_modifier.object = objTool
 
+    # Setup objTool display as wireframe
+    bpy.data.objects[objTool.name].display_type = 'WIRE'
+
+# Main
+boolean_operation(bpy.data.objects['Cylinder'])
+print('boolean_operation: Done!')
 
 
